b555d026aeccadb36cb25f1beb667dc060395bf3
jaime-wang <90151828+jaime-wang@users.noreply.github.com>
2021-11-04 21:07:04 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+url = "https://raw.githubusercontent.com/CS-521-A3/final-project-jaime-wang-and-Hilda-Guo/main/House%20Prediction%20Data.csv"
+df = pd.read_csv(url)
+print(df)
+
