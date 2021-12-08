b3622acdff9491edbe66b34f5648b7c61333ccea
jaime-wang <90151828+jaime-wang@users.noreply.github.com>
2021-12-08 18:48:56 -0500

    1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
+import sklearn
 url = "https://raw.githubusercontent.com/CS-521-A3/final-project-jaime-wang-and-Hilda-Guo/main/House%20Prediction%20Data.csv"
 df = pd.read_csv(url)
-print(df)
+df.to_csv('house_price', index=False)
 
